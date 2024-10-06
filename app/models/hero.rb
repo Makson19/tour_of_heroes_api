c10548056164b6570a9e96c6e2f5d0c22f4ceca6
Makson19 <makson.douglas@gmail.com>
2024-10-06 15:31:23 -0300

    add namespace Api and sorted heroes

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,3 +1,6 @@
 class Hero < ApplicationRecord
   validates :name, presence: true
+
+  scope :sorted_by_name, -> { order(:name) }  # ascedente
+  # scope :sorted_by_name, -> { order(name: :desc) }  # descendente
 end
